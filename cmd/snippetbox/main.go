499c092f4c0ffa0540c2000bb46fb414dd2263d8
bytemeprod <vavilov.main@icloud.com>
2025-10-06 08:16:04 +0300

    refactor: change structure of code

--- cmd/snippetbox/main.go
+++ cmd/snippetbox/main.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+
+	"github.com/bytemeprod/snippetbox/internal/handlers"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	w.Write([]byte("Hello, this is home page"))
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Write([]byte("Creating snippet..."))
-}
-
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a snippet with id %d", id)
-}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/", handlers.Home)
+	mux.HandleFunc("/snippet/create", handlers.SnippetCreate)
+	mux.HandleFunc("/snippet/view", handlers.SnippetView)
 
 	log.Println("Server starting on :4000")
 
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello, this is home page"))
+}
+
+func SnippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Creating snippet..."))
+}
+
+func SnippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a snippet with id %d", id)
+}
