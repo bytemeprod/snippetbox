0bffe5b7eb7dd95d418847ea3daca132b1965d9e
bytemeprod <vavilov.main@icloud.com>
2026-01-10 20:12:05 +0300

    feat: add mux and basic handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+const SERVER_ADDR = "localhost:8080"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("home"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("view snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create snippet"))
+}
+
 func main() {
-	
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("GET /view", snippetView)
+	mux.HandleFunc("POST /create", snippetCreate)
+
+	log.Printf("Starting server (%s)\n", SERVER_ADDR)
+	http.ListenAndServe(SERVER_ADDR, mux)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bytemeprod/snippetbox
+
+go 1.25.5
